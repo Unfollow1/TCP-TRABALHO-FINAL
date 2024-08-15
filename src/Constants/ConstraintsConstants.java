3404ce438bac2045894cd0be7778f9c2665a169d
Unfollow1 <109428575+Unfollow1@users.noreply.github.com>
2024-08-15 04:35:55 -0300

    UI fix

--- src/Constants/ConstraintsConstants.java
+++ src/Constants/ConstraintsConstants.java
@@ -4,8 +4,8 @@ public class ConstraintsConstants {
     public static final String DEFAULT_FILE_NAME = "music_generated";
     public static final String USER_INTERFACE_PATH = "UserInterface/userInterface.fxml";
     public static final String USER_INTERFACE_WINDOW_NAME = "Jukebox";
-    public static final int WINDOW_WIDTH = 779;
-    public static final int WINDOW_HEIGHT = 695;
+    public static final int WINDOW_WIDTH = 1280;
+    public static final int WINDOW_HEIGHT = 720;
     public static final int MAX_TEXT_SIZE = 1000;
     public static final int INVALID_BPM = -1;
     public static final int MIN_OCTAVE = 0;
