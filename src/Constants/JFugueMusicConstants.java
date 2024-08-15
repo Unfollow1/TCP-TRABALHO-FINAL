83ec8b54acaccd6278a351b30b19a231f170361c
Lucca Claus <luccaclaus@hotmail.com>
2024-08-15 02:45:53 -0300

    added hapsicord value constant

--- src/Constants/JFugueMusicConstants.java
+++ src/Constants/JFugueMusicConstants.java
@@ -6,4 +6,5 @@ public class JFugueMusicConstants {
     public static final String INSTRUMENT = "I";
     public static final String VOLUME = ":CON(7,";
     public static final String CLOSE_PARENTHESIS = ")";
+    public static final int HARPSICORD_VALUE = 6;
 }
