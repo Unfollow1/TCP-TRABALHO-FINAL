22847b66e825d9b103b8e7e1b8ae9c463d169ec9
Murilo Sterchile <murilosterchile@hotmail.com>
2024-08-15 03:13:09 -0300

    refatoração Main

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource(ConstraintsConstants.USER_INTERFACE_PATH));
-        primaryStage.setTitle(ConstraintsConstants.USER_INTERFACE_WINDOW_NAME);
-        primaryStage.setScene(new Scene(root, ConstraintsConstants.WINDOW_WIDTH, ConstraintsConstants.WINDOW_HEIGHT));
+    public void start(Stage primaryStage) throws Exception {
+        initializePrimaryStage(primaryStage);
         primaryStage.show();
+    }
+
+    private void initializePrimaryStage(Stage primaryStage) throws Exception {
+        Parent root = loadFXML();
+        primaryStage.setTitle(ConstraintsConstants.USER_INTERFACE_WINDOW_NAME);
+        primaryStage.setScene(createScene(root));
+    }
+
+    private Parent loadFXML() throws Exception {
+        return FXMLLoader.load(getClass().getResource(ConstraintsConstants.USER_INTERFACE_PATH));
+    }
+
+    private Scene createScene(Parent root) {
+        return new Scene(root, ConstraintsConstants.WINDOW_WIDTH, ConstraintsConstants.WINDOW_HEIGHT);
     }
 
     public static void main(String[] args) {
